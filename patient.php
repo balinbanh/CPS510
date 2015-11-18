81d9cad9a5222943050e3ef463dc4c991ed8b8b2
balinbanh <balinbanh@gmail.com>
2015-11-18 18:09:39 -0500

    Commiitting so I can sync

--- patient.php
+++ patient.php
@@ -29,231 +29,38 @@
           </tr>
         </thead>
         <tbody>
-
+		<form action = "editPatient.php">
         <?php
-         //if user is admin, loop .. replace Jack, Daniels, etc with $first, $last.. etc
-          echo "<tr>";
-          echo "<td id=\"first\" contenteditable=\"true\">Jack</td>";
-          echo "<td id=\"last\" contenteditable=\"true\">Daniels</td>";
-          echo "<td id=\"gender\" contenteditable=\"true\">M</td>";
-          echo "<td id=\"birth\" contenteditable=\"true\">10-Oct-89</td>";
-          echo "<td id=\"sNum\" contenteditable=\"true\">12</td>";
-          echo "<td id=\"street\" contenteditable=\"true\">Church St</td>";
-          echo "<td id=\"city\" contenteditable=\"true\">Toronto</td>";
-          echo "<td id=\"ohip\" contenteditable=\"true\">123821</td>";
-          echo "<td id=\"pNum\" contenteditable=\"true\">6125326161</td>";
-          echo "<td id=\"doctor\" contenteditable=\"true\">1003</td>";
-          echo "</tr>";
-
-          //extra row example
-          echo "<tr>";
-          echo "<td id=\"first\" contenteditable=\"true\">Beverly</td>";
-          echo "<td id=\"last\" contenteditable=\"true\">Li</td>";
-          echo "<td id=\"gender\" contenteditable=\"true\">F</td>";
-          echo "<td id=\"birth\" contenteditable=\"true\">30-Nov-93</td>";
-          echo "<td id=\"sNum\" contenteditable=\"true\">116</td>";
-          echo "<td id=\"street\" contenteditable=\"true\">Shaftsbury</td>";
-          echo "<td id=\"city\" contenteditable=\"true\">Richmond Hill</td>";
-          echo "<td id=\"ohip\" contenteditable=\"true\">238213</td>";
-          echo "<td id=\"pNum\" contenteditable=\"true\">6125326161</td>";
-          echo "<td id=\"doctor\" contenteditable=\"true\">1001</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
-
-          //if user is not admin, loop this
-          echo "<tr>";
-          echo "<td>Jack</td>";
-          echo "<td>Daniels</td>";
-          echo "<td>M</td>";
-          echo "<td>10-Oct-89</td>";
-          echo "<td>12</td>";
-          echo "<td>Church St</td>";
-          echo "<td>Toronto</td>";
-          echo "<td>123821</td>";
-          echo "<td>6125326161</td>";
-          echo "<td>1003</td>";
-          echo "</tr>";
+        require ("../cps510/connect.php");
+		$stid = oci_parse($conn, "SELECT * FROM PATIENTS");
+		if (!$stid) {
+		  $e = oci_error($conn);
+		  trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		}
+		// Perform the logic of the query
+		$r = oci_execute($stid);
+		if (!$r) {
+		  $e = oci_error($stid);
+		  //trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		  //echo "dead";
+		  
+		}
+        $rows;
+			while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_LOBS))
+				$rows[] = $row;
+			//echo "<table>";
+			echo count($rows);
+			foreach ($rows as $row)
+			{
+				echo '<tr>';
+				foreach ($row as $key => $value)
+				echo "<td><input type = \"text\" value = $value></td>"; 
+					//echo "<td>$value</td>";  //if they not admin cant edit
+				echo "</tr>";
+			}
+			//echo "</table>";
+			oci_free_statement($stid);
+			oci_close($conn);
         ?>
 
         </tbody>
@@ -263,9 +70,9 @@
     <div class="container">
       <br>
       <br>
-      <input type="button" class="btn btn-primary" value="Save Changes">
+      <input type="submit" class="btn btn-primary" value="Save Changes">
       <a href="addPatient.html" target="iframe_a" class="btn btn-primary">  Add Patient</a>
     </div>
-
+    </form>
   </body>
   </html>
