b4fa3296e0be9edc24bb1269c7ab5bf82aacfa1c
beverlily <beverly.li@live.com>
2015-11-27 22:15:39 -0500

    View adv/simple query down menu

--- oracle/index.php
+++ oracle/index.php
@@ -30,16 +30,42 @@
 			<input type="submit" class="btn btn-primary" value="Populate Tables">
 		</form>
 		
-		<form align = "center" name="queryTables" method="post" action="query.php">
-			<input type="submit" class="btn btn-primary" value="Query Tables">
+        <form align = "center" name="advQueries" method ="post" action="simpQuery.php">
+			<select name="Simple Queries">
+			<option value="SQ1">1</option>
+			<option value="SQ2">2</option>
+			<option value="SQ3">3</option>
+			<option value="SQ4">4</option>
+			<option value="SQ5">5</option>
+			<option value="SQ6">6</option>
+			<option value="SQ7">7</option>
+			<option value="SQ8">8</option>
+			<option value="SQ9">9</option>
+			<option value="SQ10">10</option>
+			</select>
+			<br>
+		<input type="submit" value="View Simple Query">
 		</form>
 		
+		<form align = "center" name="advQueries" method ="post" action="advQuery.php">
+			<select name="Advanced Queries">
+			<option value="AQ1">1</option>
+			<option value="AQ2">2</option>
+			<option value="AQ3">3</option>
+			<option value="AQ4">4</option>
+			<option value="AQ5">5</option>
+			<option value="AQ6">6</option>
+			<option value="AQ7">7</option>
+			</select>
+			<br>
+		<input type="submit" value="  View Advanced Query">
+		</form>
+		
+		<br>
 		<form align = "center" name="stopDB" method="post" action="stop.php">
 			<input type="submit" class="btn btn-primary" value="Force/Stop/Kill Orable DB">
 		</form>
-		
-      </div>
-    </div>
-  </form>
+		</div>
+ 
   </body>
   </html>
